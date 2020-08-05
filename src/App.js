f24939f388b8cf2669f860f25130aeba0e7a844c
mohamedsaad <mahameds19999@gmail.com>
2020-08-05 12:36:42 +0200

    Setup routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import routes from "globals/routes";
+// import { darkThemePonyfill, lightThemePonyfill } from "globals/cssVarsPonyfill";
+
+import Layout from "shared/layout";
 
 class App extends React.Component {
+  renderSingleRoute = (key, path, component) => (
+    <Route key={`route${key}`} exact path={path} component={component} />
+  );
+
   render() {
-    return <h1>Book store app is running !</h1>;
+    return (
+      <Router>
+        <Layout>
+          <Switch>
+            {routes.map((route, index) =>
+              this.renderSingleRoute(index, route.path, route.component)
+            )}
+            <Redirect from="*" to="/404" />
+          </Switch>
+        </Layout>
+      </Router>
+    );
   }
 }
 
