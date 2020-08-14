ef09c818afaf8f27ff6d180c6baf9d2fa96019e9
mohamedsaad <mahameds19999@gmail.com>
2020-08-14 21:15:22 +0200

    backup

--- src/modules/books/components/bookDetails/index.js
+++ src/modules/books/components/bookDetails/index.js
@@ -129,40 +129,139 @@ export default class BookDetails extends Component {
     return (
       <div className={book_details}>
         <div className="container">
-          <div className="row">
-            <div className="col-lg-4 col-md-6 co-sm-12">
-              <img
-                className={book_image}
-                src={this.state.book.cover}
-                alt="book photo"
-              />
-            </div>
-            <div className="col-lg-8 col-md-6 co-sm-12">
-              <h2>{this.state.book.name}</h2>
-              <Link to="/author/:id">
-                <span className={author}>{this.state.book.author}</span>
-              </Link>
-              <span>(author)</span>
-              <p className={book_price}>
-                <span className={old_price}>{this.state.book.old_price}</span>
-                <span className={new_price}>{this.state.book.price}</span>
-                <FontAwesomeIcon
-                  className={icon}
-                  icon={faStar}
-                ></FontAwesomeIcon>
-                <span className={rate}>{this.state.book.rate}</span>
-              </p>
-              <button className={add_btn}>
-                <FontAwesomeIcon
-                  className={cart_icon}
-                  icon={faShoppingCart}
-                ></FontAwesomeIcon>
-                add to cart
-              </button>
-              <h3>Description</h3>
-              <p className={description}>{this.state.book.description}</p>
-            </div>
-          </div>
+          {isLoading && <p className="text-center">Loading ...</p>}
+          {!!errMsg && <p className="text-center">{errMsg}</p>}
+          {book && (
+            <>
+              <div className="row">
+                <div className="col-lg-4 col-md-6 co-sm-12">
+                  <img
+                    className={book_image}
+                    src={book.cover}
+                    alt="book photo"
+                  />
+                </div>
+                <div className="col-lg-8 col-md-6 co-sm-12">
+                  <h1>{this.state.book.name}</h1>
+                  <Link to={`/author/${book.author._id}`}>
+                    <span className={author}>{book.author.name}</span>
+                  </Link>
+                  <span>(author)</span>
+                  <p className={book_price}>
+                    <span className={old_price}>
+                      {book.currency} {book.old_price}
+                    </span>
+                    <span className={new_price}>
+                      {book.currency} {book.price}
+                    </span>
+                    <FontAwesomeIcon
+                      className={icon}
+                      icon={faStar}
+                    ></FontAwesomeIcon>
+                    <span className={rate}>{book.rate}</span>
+                  </p>
+                  <button className={add_btn}>
+                    <FontAwesomeIcon
+                      className={cart_icon}
+                      icon={faShoppingCart}
+                    ></FontAwesomeIcon>
+                    add to cart
+                  </button>
+                  <h3>Description</h3>
+                  <p className={description}>{book.description}</p>
+                </div>
+              </div>
+              <section className={book__reviews}>
+                <section className={add_review_section}>
+                  <button
+                    className={review__btn}
+                    onClick={this.toggleModalState}
+                  >
+                    Add Review
+                  </button>
+                </section>
+              </section>
+              <section className={book__reviews__list}>
+                {book.reviews.length > 0 && (
+                  <h2>Reviews ({book.reviews.length})</h2>
+                )}
+                <ul className="list-unstyled">
+                  {book.reviews.length > 0 &&
+                    book.reviews.map((review) => (
+                      <li className="border-bottom p-4" key={review._id}>
+                        <h3 className="d-flex justify-content-between mb-2">
+                          <p>{review.author.name}</p>
+                          <small>
+                            <FontAwesomeIcon
+                              className={icon}
+                              icon={faStar}
+                            ></FontAwesomeIcon>
+                            {review.rate}
+                          </small>
+                        </h3>
+                        <p>{review.body}</p>
+                      </li>
+                    ))}
+                </ul>
+              </section>
+              <Modal
+                open={isModalOpened}
+                onClose={this.toggleModalState}
+                center
+                styles={{
+                  modal: {
+                    animation: `${
+                      isModalOpened
+                        ? "customEnterAnimation"
+                        : "customLeaveAnimation"
+                    } 500ms`,
+                  },
+                }}
+              >
+                <h2>Add a useful review</h2>
+                <form onSubmit={this.sendReview}>
+                  <section className="form-group">
+                    <label htmlFor="rate">Your rate from 1 to 5</label>
+                    <input
+                      className="form-control"
+                      type="range"
+                      name="reviewRate"
+                      id="rate"
+                      min="1"
+                      max="5"
+                      step="1"
+                      value={reviewRate}
+                      onChange={this.handleReviewChange}
+                    />
+                    <small>
+                      Rate: {reviewRate} - {this.returnRateMessage()}
+                    </small>
+                  </section>
+                  <section className="form-group">
+                    <label htmlFor="rateBody">Your Review</label>
+                    <textarea
+                      type="text"
+                      id="rateBody"
+                      className="form-control"
+                      name="reviewBody"
+                      value={reviewBody}
+                      onChange={this.handleReviewChange}
+                      rows="5"
+                    ></textarea>
+                  </section>
+                  <section className="form-group">
+                    <button
+                      type="submit"
+                      className="btn btn-primary"
+                      disabled={isSubmitting}
+                    >
+                      {isSubmitting ? "Submitting your review ..." : "Submit"}
+                    </button>
+                  </section>
+                </form>
+              </Modal>
+            </>
+          )}
         </div>
       </div>
     );
