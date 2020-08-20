971ed9b4f5143bbf757ef49726aa97e4cccf0f8e
samaaMohamed <samaamohammed15@gmail.com>
2020-08-20 05:53:26 -0700

    Edits

--- src/modules/account/components/profile/index.js
+++ src/modules/account/components/profile/index.js
@@ -28,50 +28,52 @@ export default class Profile extends Component {
   render() {
     let { user, isLoading } = this.state;
     return (
-      <div className={`${profile} container`}>
-        <Loading isLoading={isLoading} />
-        {user ? (
-          <>
-            <section className={`${profile_intro} text-center`}>
-              <h1>{user.name}</h1>
-              <p>{user.email}</p>
-              <p>{user.phone}</p>
-              <p>{user.location}</p>
-            </section>
+      <main style={{ marginBottom: "25rem", marginTop: "10rem" }}>
+        <div className={`${profile} container`}>
+          <Loading isLoading={isLoading} />
+          {user ? (
+            <>
+              <section className={`${profile_intro} text-center`}>
+                <h1>{user.name}</h1>
+                <p>{user.email}</p>
+                <p>{user.phone}</p>
+                <p>{user.location}</p>
+              </section>
 
-            <section className={`${profile_orders} card`}>
-              <h2 className="mb-5">My orders</h2>
-              <ul className="list-unstyled">
-                {user.orders.map((order) => {
-                  if (
-                    order.status === "delivered" ||
-                    order.status === "cancelled"
-                  ) {
-                    return (
-                      <li className={order_item}>
-                        <section className="d-flex justify-content-between">
-                          <h3>{order.bookName}</h3>
-                          <p className={text_red}>{order.status}</p>
-                        </section>
-                        <p>
-                          {order.currency} {order.price}
-                        </p>
-                      </li>
-                    );
-                  } else return "";
-                })}
-              </ul>
-            </section>
-          </>
-        ) : (
-          !isLoading && (
-            <p>
-              An error occurred, please refresh or make sure to{" "}
-              <a href="/login?returnUrl=profile">login</a>
-            </p>
-          )
-        )}
-      </div>
+              <section className={`${profile_orders} card`}>
+                <h2 className="mb-5">My orders</h2>
+                <ul className="list-unstyled">
+                  {user.orders.map((order) => {
+                    if (
+                      order.status === "delivered" ||
+                      order.status === "cancelled"
+                    ) {
+                      return (
+                        <li className={order_item}>
+                          <section className="d-flex justify-content-between">
+                            <h3>{order.bookName}</h3>
+                            <p className={text_red}>{order.status}</p>
+                          </section>
+                          <p>
+                            {order.currency} {order.price}
+                          </p>
+                        </li>
+                      );
+                    } else return "";
+                  })}
+                </ul>
+              </section>
+            </>
+          ) : (
+            !isLoading && (
+              <p>
+                An error occurred, please refresh or make sure to{" "}
+                <a href="/login?returnUrl=profile">login</a>
+              </p>
+            )
+          )}
+        </div>
+      </main>
     );
   }
 }
